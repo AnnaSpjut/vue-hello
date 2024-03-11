3fa41c4778478db884ac41c3fde865a7e7156dd6
Anna Spjut <anna.spjut@gmail.com>
2024-03-11 19:26:42 +0400

    [add] bonus

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const{createApp} = Vue;
 createApp({
   data(){
     return{
-      messaggio: 'Oggi ci è stato consegnato un esercizio facile!'
+      messaggio: 'Oggi ci è stato consegnato un esercizio facile!',
+      image: "assets/img/pexels-antoni-shkraba-6827117.jpg"
     }
   }
 }).mount('#app')
